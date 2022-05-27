d8e55d7ee798ce2cec91fdcb2aa95f09c04854cd
Henrique <henrique@henriqueog.com>
2022-05-27 10:17:37 -0300

    Update webdriver_utils.py

--- webdriver_utils.py
+++ webdriver_utils.py
@@ -1,3 +1,4 @@
+""""""
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
@@ -6,76 +7,84 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
 import os
 from time import sleep
-# import win32gui, win32con
 
-# hide = win32gui.GetForegroundWindow()
-# win32gui.ShowWindow(hide,win32con.SW_HIDE)
 
-def criarWebDriver(tipoDriver):
-	"""Evita de que, quando esteja analisando e fazendo o download do webdriver, se alguma verificação de segurança será considerada"""
+
+def criarWebDriver(tipoDriver='Chrome'):
+	"""Cria uma instância de webdriver do selenium"""
 	os.environ['WDM_SSL_VERIFY']='0'
 	global opcoes
 	global driver
+	global acoes
 	driver = escolherWebDriver(tipoDriver.upper())
+	acoes = ActionChains(driver)
 	
 
 def escolherWebDriver(driverString):
-    opcoes = {
+	"""Função interna que trabalha a partir da chamada da função criarWebDriver"""
+	opcoes = {
        	'CHROME': criarChromeDriver,
        	'EDGE': criarEdgeDriver,
        	'FIREFOX':criarFirefoxDriver,
-    }
-    return opcoes.get(driverString)()
+       	}
+	return opcoes.get(driverString)()
 
 def criarChromeDriver():
+	"""Cria e retorna o webdriver do navegador Chrome"""
 	opcoes = webdriver.ChromeOptions()
 	opcoes.add_argument("start-maximized")
 	opcoes.add_experimental_option('excludeSwitches', ['enable-logging'])
 	return webdriver.Chrome(ChromeDriverManager(path=os.getcwd()).install(),options=opcoes)
 
 def criarFirefoxDriver():
+	"""Cria e retorna o webdriver do navegador Firefox"""
 	return webdriver.Firefox(GeckoDriverManager(path=os.getcwd()).install())
 
 def criarEdgeDriver():
+	"""Cria e retorna o webdriver do navegador Edge"""
 	opcoes = Options()
 	opcoes.add_argument("start-maximized")
 	return webdriver.Edge(EdgeChromiumDriverManager(path=os.getcwd()).install(),options=opcoes)
 
-
-def verificarDOMCarregado():
-    carregado = False
-    while carregado == False:
-        estadoAtual = driver.execute_script('return window.document.readyState')
-        if estadoAtual != 'complete':
-            sleep(1)
-        else:
-            carregado = True
+def aguardarPagina():
+	"""Define um wait para o carregamento da página, aguardando estar pronta para a próxima ação"""
+	yield WebDriverWait(driver, 3).until(EC.staleness_of(driver.find_element(By.TAG_NAME,'html')))
+	WebDriverWait(driver, 10).until(lambda driver: driver.execute_script("return document.readyState;") == "complete")
 
 def centralizarElemento(elemento):
-	acoes = ActionChains(driver)
+	"""Focaliza (centralizando, rolando a página para) no elemento passado como parâmetro"""
 	acoes.move_to_element(elemento).perform()
 
 def navegarPara(URL):
+	"""instruí o webdriver criado a navegar para o endereço passado como parâmetro"""
 	driver.get(URL)
-	verificarDOMCarregado()
-	print('carregou')
+	aguardarPagina()
 
 def pegaElementopeloXPath(elementoXPath):
-	return driver.find_element_by_xpath(elementoXPath)
+	"""Captura um dado elemento (o parâmetro) e o retorna"""
+	return driver.find_element(By.XPATH,elementoXPath)
 
 def clicaElementopeloXPath(elementoXPath):
-	elemento = getElementByXPath(elementoXPath)
-	acoes = ActionChains(driver)
+	"""Clica no elemento passado como parâmetro utilizando actions"""
+	elemento = pegaElementopeloXPath(elementoXPath)
 	acoes.click(elemento).perform()
-	verificarDOMCarregado()
+	aguardarPagina()
 
-def pegaConteudopeloAtributo(elemento,tipoAtributo):
-	valor=''
-	# consideraremos quando o parâmetro for vazio, para capturar o textContent como default
-	if len(tipoAtributo.strip())==0:
-		valor = elemento.get_attribute('textContent')
-	else:
-		valor = elemento.get_attribute(tipoAtributo)
+def pegaConteudopeloAtributo(elemento,tipoAtributo='textContent'):
+	"""Captura o valor do atributo do elemento passado como parâmetro"""
+	valor = elemento.get_attribute(tipoAtributo)
 	return valor
+
+def moveparaElemento(elemento):
+	"""Focaliza e move o cursor do mouse para o elemento passado como parâmetro"""
+	acoes.move_to_element(elemento).perform()
+
+def preencheInputbyXPath(elemXPath,inputValue):
+	"""Preenche o campo de input passado como parâmetro, com o valor de 'inputValue', performando a escrita através de actions"""
+	elemInput = pegaElementopeloXPath(elementoXPath)
+	centralizarElemento(elemInput)
+	acoes.click(elemInput).send_keys(inputValue.strip()).perform()
